c8e810b14a1b7dbbaf31a9df27a376b2c5c8a488
Vivek Vaish <vaishvivek634@gmail.com>
2023-08-28 08:41:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import currencies from "./country";
+import Select from "react-select";
+import axios from "axios";
+
+const API_KEY = '82ed90b1610d9b921739e28e'
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair`;
 
 function App() {
+  const [inputValue, setInputValue] = useState(1);
+  const [baseCurrency, setBaseCurrency] = useState('USD');
+  const [targetCurrency, setTargetCurrency] = useState('INR');
+  const [exchangeRates, setExchangeRates] = useState(0);
+
+  useEffect(() => {
+    getExchangeRates();
+  }, [])
+
+  useEffect(() => {
+    getExchangeRates();
+  }, [inputValue, baseCurrency, targetCurrency]);
+
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleTargetCurrency(e) {
+    setTargetCurrency(e.value);
+  }
+
+  function handleBaseCurrency(e) {
+    setBaseCurrency(e.value);
+  }
+
+  function handleChange() {
+    const temp = targetCurrency;
+    setTargetCurrency(baseCurrency);
+    setBaseCurrency(temp);
+  }
+
+  async function getExchangeRates() {
+    const response = await axios.get(`${BASE_URL}/${baseCurrency}/${targetCurrency}/${inputValue}`);
+    const result = response.data.conversion_result;
+    setExchangeRates(result);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Currency Converter</h1>
+      <div className="input-form">
+        <label>Enter Amount:</label>
+        <input type="number" value={inputValue} onChange={handleInput} />
+        <label>Base Currency:</label>
+        <Select className="selection"
+          options={currencies}
+          onChange={handleBaseCurrency}
+          value={currencies.find(curr => curr.value === baseCurrency)} />
+        <div onClick={handleChange} className="exchange">⇵</div>
+        <label>Target Currency:</label>
+        <Select className="selection"
+          options={currencies}
+          onChange={handleTargetCurrency}
+          value={currencies.find(curr => curr.value === targetCurrency)} />
+        <div className="result">{inputValue} {baseCurrency} = {exchangeRates} {targetCurrency}</div>
+      </div>
     </div>
   );
 }
---
+++ src/country.js
@@ -0,0 +1,976 @@
+const currency = [
+    {
+        "label": "Afghanistan",
+        "value": "AFN"
+    },
+    {
+        "label": "Albania",
+        "value": "ALL"
+    },
+    {
+        "label": "Algeria",
+        "value": "DZD"
+    },
+    {
+        "label": "American Samoa",
+        "value": "USD"
+    },
+    {
+        "label": "Andorra",
+        "value": "EUR"
+    },
+    {
+        "label": "Angola",
+        "value": "AOA"
+    },
+    {
+        "label": "Anguilla",
+        "value": "XCD"
+    },
+    {
+        "label": "Antarctica",
+        "value": "XCD"
+    },
+    {
+        "label": "Antigua and Barbuda",
+        "value": "XCD"
+    },
+    {
+        "label": "Argentina",
+        "value": "ARS"
+    },
+    {
+        "label": "Armenia",
+        "value": "AMD"
+    },
+    {
+        "label": "Aruba",
+        "value": "AWG"
+    },
+    {
+        "label": "Australia",
+        "value": "AUD"
+    },
+    {
+        "label": "Austria",
+        "value": "EUR"
+    },
+    {
+        "label": "Azerbaijan",
+        "value": "AZN"
+    },
+    {
+        "label": "Bahamas",
+        "value": "BSD"
+    },
+    {
+        "label": "Bahrain",
+        "value": "BHD"
+    },
+    {
+        "label": "Bangladesh",
+        "value": "BDT"
+    },
+    {
+        "label": "Barbados",
+        "value": "BBD"
+    },
+    {
+        "label": "Belarus",
+        "value": "BYR"
+    },
+    {
+        "label": "Belgium",
+        "value": "EUR"
+    },
+    {
+        "label": "Belize",
+        "value": "BZD"
+    },
+    {
+        "label": "Benin",
+        "value": "XOF"
+    },
+    {
+        "label": "Bermuda",
+        "value": "BMD"
+    },
+    {
+        "label": "Bhutan",
+        "value": "BTN"
+    },
+    {
+        "label": "Bolivia",
+        "value": "BOB"
+    },
+    {
+        "label": "Bosnia and Herzegovina",
+        "value": "BAM"
+    },
+    {
+        "label": "Botswana",
+        "value": "BWP"
+    },
+    {
+        "label": "Bouvet Island",
+        "value": "NOK"
+    },
+    {
+        "label": "Brazil",
+        "value": "BRL"
+    },
+    {
+        "label": "British Indian Ocean Territory",
+        "value": "USD"
+    },
+    {
+        "label": "Brunei",
+        "value": "BND"
+    },
+    {
+        "label": "Bulgaria",
+        "value": "BGN"
+    },
+    {
+        "label": "Burkina Faso",
+        "value": "XOF"
+    },
+    {
+        "label": "Burundi",
+        "value": "BIF"
+    },
+    {
+        "label": "Cambodia",
+        "value": "KHR"
+    },
+    {
+        "label": "Cameroon",
+        "value": "XAF"
+    },
+    {
+        "label": "Canada",
+        "value": "CAD"
+    },
+    {
+        "label": "Cape Verde",
+        "value": "CVE"
+    },
+    {
+        "label": "Cayman Islands",
+        "value": "KYD"
+    },
+    {
+        "label": "Central African Republic",
+        "value": "XAF"
+    },
+    {
+        "label": "Chad",
+        "value": "XAF"
+    },
+    {
+        "label": "Chile",
+        "value": "CLP"
+    },
+    {
+        "label": "China",
+        "value": "CNY"
+    },
+    {
+        "label": "Christmas Island",
+        "value": "AUD"
+    },
+    {
+        "label": "Cocos (Keeling) Islands",
+        "value": "AUD"
+    },
+    {
+        "label": "Colombia",
+        "value": "COP"
+    },
+    {
+        "label": "Comoros",
+        "value": "KMF"
+    },
+    {
+        "label": "Congo",
+        "value": "XAF"
+    },
+    {
+        "label": "Cook Islands",
+        "value": "NZD"
+    },
+    {
+        "label": "Costa Rica",
+        "value": "CRC"
+    },
+    {
+        "label": "Croatia",
+        "value": "HRK"
+    },
+    {
+        "label": "Cuba",
+        "value": "CUP"
+    },
+    {
+        "label": "Cyprus",
+        "value": "EUR"
+    },
+    {
+        "label": "Czech Republic",
+        "value": "CZK"
+    },
+    {
+        "label": "Denmark",
+        "value": "DKK"
+    },
+    {
+        "label": "Djibouti",
+        "value": "DJF"
+    },
+    {
+        "label": "Dominica",
+        "value": "XCD"
+    },
+    {
+        "label": "Dominican Republic",
+        "value": "DOP"
+    },
+    {
+        "label": "East Timor",
+        "value": "USD"
+    },
+    {
+        "label": "Ecuador",
+        "value": "ECS"
+    },
+    {
+        "label": "Egypt",
+        "value": "EGP"
+    },
+    {
+        "label": "El Salvador",
+        "value": "SVC"
+    },
+    {
+        "label": "England",
+        "value": "GBP"
+    },
+    {
+        "label": "Equatorial Guinea",
+        "value": "XAF"
+    },
+    {
+        "label": "Eritrea",
+        "value": "ERN"
+    },
+    {
+        "label": "Estonia",
+        "value": "EUR"
+    },
+    {
+        "label": "Ethiopia",
+        "value": "ETB"
+    },
+    {
+        "label": "Falkland Islands",
+        "value": "FKP"
+    },
+    {
+        "label": "Faroe Islands",
+        "value": "DKK"
+    },
+    {
+        "label": "Fiji Islands",
+        "value": "FJD"
+    },
+    {
+        "label": "Finland",
+        "value": "EUR"
+    },
+    {
+        "label": "France",
+        "value": "EUR"
+    },
+    {
+        "label": "French Guiana",
+        "value": "EUR"
+    },
+    {
+        "label": "French Polynesia",
+        "value": "XPF"
+    },
+    {
+        "label": "French Southern territories",
+        "value": "EUR"
+    },
+    {
+        "label": "Gabon",
+        "value": "XAF"
+    },
+    {
+        "label": "Gambia",
+        "value": "GMD"
+    },
+    {
+        "label": "Georgia",
+        "value": "GEL"
+    },
+    {
+        "label": "Germany",
+        "value": "EUR"
+    },
+    {
+        "label": "Ghana",
+        "value": "GHS"
+    },
+    {
+        "label": "Gibraltar",
+        "value": "GIP"
+    },
+    {
+        "label": "Greece",
+        "value": "EUR"
+    },
+    {
+        "label": "Greenland",
+        "value": "DKK"
+    },
+    {
+        "label": "Grenada",
+        "value": "XCD"
+    },
+    {
+        "label": "Guadeloupe",
+        "value": "EUR"
+    },
+    {
+        "label": "Guam",
+        "value": "USD"
+    },
+    {
+        "label": "Guatemala",
+        "value": "QTQ"
+    },
+    {
+        "label": "Guinea",
+        "value": "GNF"
+    },
+    {
+        "label": "Guinea-Bissau",
+        "value": "CFA"
+    },
+    {
+        "label": "Guyana",
+        "value": "GYD"
+    },
+    {
+        "label": "Haiti",
+        "value": "HTG"
+    },
+    {
+        "label": "Heard Island and McDonald Islands",
+        "value": "AUD"
+    },
+    {
+        "label": "Holy See (Vatican City State)",
+        "value": "EUR"
+    },
+    {
+        "label": "Honduras",
+        "value": "HNL"
+    },
+    {
+        "label": "Hong Kong",
+        "value": "HKD"
+    },
+    {
+        "label": "Hungary",
+        "value": "HUF"
+    },
+    {
+        "label": "Iceland",
+        "value": "ISK"
+    },
+    {
+        "label": "India",
+        "value": "INR"
+    },
+    {
+        "label": "Indonesia",
+        "value": "IDR"
+    },
+    {
+        "label": "Iran",
+        "value": "IRR"
+    },
+    {
+        "label": "Iraq",
+        "value": "IQD"
+    },
+    {
+        "label": "Ireland",
+        "value": "EUR"
+    },
+    {
+        "label": "Israel",
+        "value": "ILS"
+    },
+    {
+        "label": "Italy",
+        "value": "EUR"
+    },
+    {
+        "label": "Ivory Coast",
+        "value": "XOF"
+    },
+    {
+        "label": "Jamaica",
+        "value": "JMD"
+    },
+    {
+        "label": "Japan",
+        "value": "JPY"
+    },
+    {
+        "label": "Jordan",
+        "value": "JOD"
+    },
+    {
+        "label": "Kazakhstan",
+        "value": "KZT"
+    },
+    {
+        "label": "Kenya",
+        "value": "KES"
+    },
+    {
+        "label": "Kiribati",
+        "value": "AUD"
+    },
+    {
+        "label": "Kuwait",
+        "value": "KWD"
+    },
+    {
+        "label": "Kyrgyzstan",
+        "value": "KGS"
+    },
+    {
+        "label": "Laos",
+        "value": "LAK"
+    },
+    {
+        "label": "Latvia",
+        "value": "LVL"
+    },
+    {
+        "label": "Lebanon",
+        "value": "LBP"
+    },
+    {
+        "label": "Lesotho",
+        "value": "LSL"
+    },
+    {
+        "label": "Liberia",
+        "value": "LRD"
+    },
+    {
+        "label": "Libyan Arab Jamahiriya",
+        "value": "LYD"
+    },
+    {
+        "label": "Liechtenstein",
+        "value": "CHF"
+    },
+    {
+        "label": "Lithuania",
+        "value": "LTL"
+    },
+    {
+        "label": "Luxembourg",
+        "value": "EUR"
+    },
+    {
+        "label": "Macau",
+        "value": "MOP"
+    },
+    {
+        "label": "North Macedonia",
+        "value": "MKD"
+    },
+    {
+        "label": "Madagascar",
+        "value": "MGF"
+    },
+    {
+        "label": "Malawi",
+        "value": "MWK"
+    },
+    {
+        "label": "Malaysia",
+        "value": "MYR"
+    },
+    {
+        "label": "Maldives",
+        "value": "MVR"
+    },
+    {
+        "label": "Mali",
+        "value": "XOF"
+    },
+    {
+        "label": "Malta",
+        "value": "EUR"
+    },
+    {
+        "label": "Marshall Islands",
+        "value": "USD"
+    },
+    {
+        "label": "Martinique",
+        "value": "EUR"
+    },
+    {
+        "label": "Mauritania",
+        "value": "MRO"
+    },
+    {
+        "label": "Mauritius",
+        "value": "MUR"
+    },
+    {
+        "label": "Mayotte",
+        "value": "EUR"
+    },
+    {
+        "label": "Mexico",
+        "value": "MXN"
+    },
+    {
+        "label": "Micronesia, Federated States of",
+        "value": "USD"
+    },
+    {
+        "label": "Moldova",
+        "value": "MDL"
+    },
+    {
+        "label": "Monaco",
+        "value": "EUR"
+    },
+    {
+        "label": "Mongolia",
+        "value": "MNT"
+    },
+    {
+        "label": "Montserrat",
+        "value": "XCD"
+    },
+    {
+        "label": "Morocco",
+        "value": "MAD"
+    },
+    {
+        "label": "Mozambique",
+        "value": "MZN"
+    },
+    {
+        "label": "Myanmar",
+        "value": "MMR"
+    },
+    {
+        "label": "Namibia",
+        "value": "NAD"
+    },
+    {
+        "label": "Nauru",
+        "value": "AUD"
+    },
+    {
+        "label": "Nepal",
+        "value": "NPR"
+    },
+    {
+        "label": "Netherlands",
+        "value": "EUR"
+    },
+    {
+        "label": "Netherlands Antilles",
+        "value": "ANG"
+    },
+    {
+        "label": "New Caledonia",
+        "value": "XPF"
+    },
+    {
+        "label": "New Zealand",
+        "value": "NZD"
+    },
+    {
+        "label": "Nicaragua",
+        "value": "NIO"
+    },
+    {
+        "label": "Niger",
+        "value": "XOF"
+    },
+    {
+        "label": "Nigeria",
+        "value": "NGN"
+    },
+    {
+        "label": "Niue",
+        "value": "NZD"
+    },
+    {
+        "label": "Norfolk Island",
+        "value": "AUD"
+    },
+    {
+        "label": "North Korea",
+        "value": "KPW"
+    },
+    {
+        "label": "Northern Ireland",
+        "value": "GBP"
+    },
+    {
+        "label": "Northern Mariana Islands",
+        "value": "USD"
+    },
+    {
+        "label": "Norway",
+        "value": "NOK"
+    },
+    {
+        "label": "Oman",
+        "value": "OMR"
+    },
+    {
+        "label": "Pakistan",
+        "value": "PKR"
+    },
+    {
+        "label": "Palau",
+        "value": "USD"
+    },
+    {
+        "label": "Palestine",
+        "value": null
+    },
+    {
+        "label": "Panama",
+        "value": "PAB"
+    },
+    {
+        "label": "Papua New Guinea",
+        "value": "PGK"
+    },
+    {
+        "label": "Paraguay",
+        "value": "PYG"
+    },
+    {
+        "label": "Peru",
+        "value": "PEN"
+    },
+    {
+        "label": "Philippines",
+        "value": "PHP"
+    },
+    {
+        "label": "Pitcairn Islands",
+        "value": "NZD"
+    },
+    {
+        "label": "Poland",
+        "value": "PLN"
+    },
+    {
+        "label": "Portugal",
+        "value": "EUR"
+    },
+    {
+        "label": "Puerto Rico",
+        "value": "USD"
+    },
+    {
+        "label": "Qatar",
+        "value": "QAR"
+    },
+    {
+        "label": "Reunion",
+        "value": "EUR"
+    },
+    {
+        "label": "Romania",
+        "value": "RON"
+    },
+    {
+        "label": "Russian Federation",
+        "value": "RUB"
+    },
+    {
+        "label": "Rwanda",
+        "value": "RWF"
+    },
+    {
+        "label": "Saint Helena",
+        "value": "SHP"
+    },
+    {
+        "label": "Saint Kitts and Nevis",
+        "value": "XCD"
+    },
+    {
+        "label": "Saint Lucia",
+        "value": "XCD"
+    },
+    {
+        "label": "Saint Pierre and Miquelon",
+        "value": "EUR"
+    },
+    {
+        "label": "Saint Vincent and the Grenadines",
+        "value": "XCD"
+    },
+    {
+        "label": "Samoa",
+        "value": "WST"
+    },
+    {
+        "label": "San Marino",
+        "value": "EUR"
+    },
+    {
+        "label": "Sao Tome and Principe",
+        "value": "STD"
+    },
+    {
+        "label": "Saudi Arabia",
+        "value": "SAR"
+    },
+    {
+        "label": "Scotland",
+        "value": "GBP"
+    },
+    {
+        "label": "Senegal",
+        "value": "XOF"
+    },
+    {
+        "label": "Serbia",
+        "value": "RSD"
+    },
+    {
+        "label": "Seychelles",
+        "value": "SCR"
+    },
+    {
+        "label": "Sierra Leone",
+        "value": "SLL"
+    },
+    {
+        "label": "Singapore",
+        "value": "SGD"
+    },
+    {
+        "label": "Slovakia",
+        "value": "EUR"
+    },
+    {
+        "label": "Slovenia",
+        "value": "EUR"
+    },
+    {
+        "label": "Solomon Islands",
+        "value": "SBD"
+    },
+    {
+        "label": "Somalia",
+        "value": "SOS"
+    },
+    {
+        "label": "South Africa",
+        "value": "ZAR"
+    },
+    {
+        "label": "South Georgia and the South Sandwich Islands",
+        "value": "GBP"
+    },
+    {
+        "label": "South Korea",
+        "value": "KRW"
+    },
+    {
+        "label": "South Sudan",
+        "value": "SSP"
+    },
+    {
+        "label": "Spain",
+        "value": "EUR"
+    },
+    {
+        "label": "Sri Lanka",
+        "value": "LKR"
+    },
+    {
+        "label": "Sudan",
+        "value": "SDG"
+    },
+    {
+        "label": "Suriname",
+        "value": "SRD"
+    },
+    {
+        "label": "Svalbard and Jan Mayen",
+        "value": "NOK"
+    },
+    {
+        "label": "Swaziland",
+        "value": "SZL"
+    },
+    {
+        "label": "Sweden",
+        "value": "SEK"
+    },
+    {
+        "label": "Switzerland",
+        "value": "CHF"
+    },
+    {
+        "label": "Syria",
+        "value": "SYP"
+    },
+    {
+        "label": "Tajikistan",
+        "value": "TJS"
+    },
+    {
+        "label": "Tanzania",
+        "value": "TZS"
+    },
+    {
+        "label": "Thailand",
+        "value": "THB"
+    },
+    {
+        "label": "The Democratic Republic of Congo",
+        "value": "CDF"
+    },
+    {
+        "label": "Togo",
+        "value": "XOF"
+    },
+    {
+        "label": "Tokelau",
+        "value": "NZD"
+    },
+    {
+        "label": "Tonga",
+        "value": "TOP"
+    },
+    {
+        "label": "Trinidad and Tobago",
+        "value": "TTD"
+    },
+    {
+        "label": "Tunisia",
+        "value": "TND"
+    },
+    {
+        "label": "Turkey",
+        "value": "TRY"
+    },
+    {
+        "label": "Turkmenistan",
+        "value": "TMT"
+    },
+    {
+        "label": "Turks and Caicos Islands",
+        "value": "USD"
+    },
+    {
+        "label": "Tuvalu",
+        "value": "AUD"
+    },
+    {
+        "label": "Uganda",
+        "value": "UGX"
+    },
+    {
+        "label": "Ukraine",
+        "value": "UAH"
+    },
+    {
+        "label": "United Arab Emirates",
+        "value": "AED"
+    },
+    {
+        "label": "United Kingdom",
+        "value": "GBP"
+    },
+    {
+        "label": "United States",
+        "value": "USD"
+    },
+    {
+        "label": "United States Minor Outlying Islands",
+        "value": "USD"
+    },
+    {
+        "label": "Uruguay",
+        "value": "UYU"
+    },
+    {
+        "label": "Uzbekistan",
+        "value": "UZS"
+    },
+    {
+        "label": "Vanuatu",
+        "value": "VUV"
+    },
+    {
+        "label": "Venezuela",
+        "value": "VEF"
+    },
+    {
+        "label": "Vietnam",
+        "value": "VND"
+    },
+    {
+        "label": "Virgin Islands, British",
+        "value": "USD"
+    },
+    {
+        "label": "Virgin Islands, U.S.",
+        "value": "USD"
+    },
+    {
+        "label": "Wales",
+        "value": "GBP"
+    },
+    {
+        "label": "Wallis and Futuna",
+        "value": "XPF"
+    },
+    {
+        "label": "Western Sahara",
+        "value": "MAD"
+    },
+    {
+        "label": "Yemen",
+        "value": "YER"
+    },
+    {
+        "label": "Zambia",
+        "value": "ZMW"
+    },
+    {
+        "label": "Zimbabwe",
+        "value": "ZWD"
+    }
+]
+
+module.exports = currency;
